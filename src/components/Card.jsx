e5deaa22775b7c5baa20b38617e1c86c7af7d966
Renan N Borges <renan.n.borges@hotmail.com>
2025-11-10 13:25:34 -0300

    reset front-end

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-export default function Card(props) {
-    return (
-        <div>
-            <img src={props.img} alt="Card"/>
-            <h3>{props.title}</h3>
-            <p>{props.content}</p>
-        </div>
-    )
-}
--- src/components/Home/HomeContainer.jsx
+++
@@ -1,9 +0,0 @@
-export default function HomeContainer({ children, content='' }) {
-    return (
-        <div>
-            <h1>{children}</h1>
-            <p>{content}</p>
-        </div>
-
-    )
-}
